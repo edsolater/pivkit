36949acf4effad0c50c646855be0f6060ff8837e
zhanggongying <edsolater@qq.com>
2024-04-05 17:43:41 +0800

    🚧 improve raF

--- src/domkit/utils/requestLoopAnimationFrame.ts
+++ src/domkit/utils/requestLoopAnimationFrame.ts
@@ -2,25 +2,41 @@
  * @todo option:endUntil、option:eachMS、option:eachFrameCount
  */
 export function requestLoopAnimationFrame(
-  fn: FrameRequestCallback,
+  fn: (payloads: { pasedTime: number | undefined }) => void,
   options?: {
     /** if ture, cancel the frame loop */
     endUntil?: () => boolean
-    eachMS?: number
   },
 ) {
   let rAFId: number
+  let prevTime: number | undefined = undefined
   const frameCallback = (...args: Parameters<FrameRequestCallback>) => {
-    fn(...args)
-    globalThis.requestAnimationFrame(frameCallback)
+    const [time] = args
+    const pastTime = prevTime != null ? time - prevTime : undefined
+    prevTime = time
+    fn({ pasedTime: pastTime })
+    rAFId = globalThis.requestAnimationFrame(frameCallback)
   }
   rAFId = globalThis.requestAnimationFrame(frameCallback)
-  return {
-    rAFId() {
-      return rAFId
-    },
-    cancel() {
-      return globalThis.cancelAnimationFrame(rAFId)
-    },
+  function getCurrentPastTime() {
+    return prevTime
   }
+  function cancel() {
+    return globalThis.cancelAnimationFrame(rAFId)
+  }
+  function getAFId() {
+    return rAFId
+  }
+  const manager = {
+    get pastTime() {
+      return getCurrentPastTime()
+    },
+    getCurrentPastTime,
+    cancel,
+    get rAFId() {
+      return getAFId()
+    },
+    getAFId,
+  }
+  return manager
 }
