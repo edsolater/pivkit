d603c1e7550ef12f25677e4132914fff3592e649
edsolater <edsolater@qq.com>
2024-04-25 01:10:25 +0800

    🩹 sortablePlugin

--- src/domkit/utils/moveElementDOMToNewContiner.ts
+++ src/domkit/utils/moveElementDOMToNewContiner.ts
@@ -14,5 +14,35 @@ export function moveElementNextToSibling({
   dragElement: HTMLElement
   leaderElement: HTMLElement
 }) {
-  leaderElement.after(dragElement)
+  const dragElementRect = dragElement.getBoundingClientRect()
+  const leaderElementRect = leaderElement.getBoundingClientRect()
+  const dir = (() => {
+    const dragElementCenterPoint = {
+      x: dragElementRect.left + dragElementRect.width / 2,
+      y: dragElementRect.top + dragElementRect.height / 2,
+    }
+    const leaderElementCenterPoint = {
+      x: leaderElementRect.left + leaderElementRect.width / 2,
+      y: leaderElementRect.top + leaderElementRect.height / 2,
+    }
+    const deltaX = Math.abs(leaderElementCenterPoint.x - dragElementCenterPoint.x)
+    const deltaY = Math.abs(leaderElementCenterPoint.y - dragElementCenterPoint.y)
+    if (deltaX > deltaY) {
+      return "horizontal"
+    } else {
+      return "vertical"
+    }
+  })()
+  const leaderShouldAppearFirst = (() => {
+    if (dir === "horizontal") {
+      return dragElementRect.left < leaderElementRect.left
+    } else {
+      return dragElementRect.top < leaderElementRect.top
+    }
+  })()
+  if (leaderShouldAppearFirst) {
+    leaderElement.before(dragElement)
+  } else {
+    leaderElement.after(dragElement)
+  }
 }
