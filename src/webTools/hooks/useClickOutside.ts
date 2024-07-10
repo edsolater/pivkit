d96bc9feba0459f49b0b599e832da6b617642f91
edsolater <edsolater@qq.com>
2024-07-10 22:51:55 +0800

    🐛 `useClickOutside` not work basic

--- src/webTools/hooks/useClickOutside.ts
+++ src/webTools/hooks/useClickOutside.ts
@@ -16,20 +16,19 @@ export type UseClickOutsideOptions =
   | OnClickOutSideCallback
 
 /**
- * TODO: should click event be bubbled to root? current too many click event 
  * inner use (bubbled to root) click event's `event.composedPath()` to check if the click event is outside of the target elements
  * @param els can be a single element or an array of elements or even a function that returns an element or an array of elements
  * @param options
  */
 export function useClickOutside(els: ElementRefs, options?: UseClickOutsideOptions) {
   const parasedOptions = typeof options === "function" ? { onClickOutSide: options } : options
-  const getOption = () => parasedOptions
+
   createEffect(() => {
     const targetElements = getElementFromRefs(els)
     targetElements.forEach((el) => {
-      const { cancel } = listenDomEvent(el, "click", (payload) => {
-        const enabled = shrinkFn(getOption()?.enabled)
-        const disabled = shrinkFn(getOption()?.disabled)
+      const { cancel } = listenDomEvent(document, "click", (payload) => {
+        const enabled = shrinkFn(parasedOptions?.enabled)
+        const disabled = shrinkFn(parasedOptions?.disabled)
         const isEnabled = enabled != null ? enabled : !disabled
         if (!isEnabled) return
 
