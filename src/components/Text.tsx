a4028b1c14a282dbed6ef9e314690d9c3cec5f48
edsolater <edsolater@qq.com>
2024-08-10 23:19:06 +0800

    🐛 fix `<Text>` have easy and complex mode

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -3,7 +3,7 @@ import { createEffect, createMemo, createSignal, on } from "solid-js"
 import { KitProps, useKitProps } from "../createKit"
 import { Piv, parseICSSToClassName } from "../piv"
 
-export interface TextRawProps {
+export interface TextProps {
   /**
    *
    *  all widgets should have `props:v`, to handle it's duty's property \
@@ -14,35 +14,45 @@ export interface TextRawProps {
   defaultValue?: string | number
 }
 
-export type TextProps = KitProps<TextRawProps>
+export type TextKitProps = KitProps<TextProps>
 
 /**
  * @componentType widget
  * if for layout , inner content should only be text
  */
-export function Text(kitProps: TextProps) {
+export function Text(kitProps: TextKitProps) {
   const { props, shadowProps } = useKitProps(kitProps, { name: "Text" })
 
-  const value = createMemo(() =>
-    "value" in props ? props.value : "children" in props && isString(props.children) ? props.children : undefined,
-  )
-  const [innerValue, setInnerValue] = createSignal<string | number | undefined>(value() ?? props.defaultValue)
+  if ("value" in kitProps || "defaultValue" in kitProps) {
+    // NOTE: is value and defaultValue in props
 
-  createEffect(
-    on(
-      value,
-      () => {
-        setInnerValue(value())
-      },
-      { defer: true },
-    ),
-  )
+    const value = createMemo(() =>
+      "value" in props ? props.value : "children" in props && isString(props.children) ? props.children : undefined,
+    )
+    const [innerValue, setInnerValue] = createSignal<string | number | undefined>(value() ?? props.defaultValue)
+    createEffect(
+      on(
+        value,
+        () => {
+          setInnerValue(value())
+        },
+        { defer: true },
+      ),
+    )
+    return (
+      <Piv shadowProps={shadowProps} icss={defaultTextICSS}>
+        {value() ?? innerValue()}
+      </Piv>
+    )
+  } else {
+    // NOTE: just use props.children to be faster
 
-  return (
-    <Piv shadowProps={shadowProps} icss={defaultTextICSS}>
-      {innerValue()}
-    </Piv>
-  )
+    return (
+      <Piv shadowProps={shadowProps} icss={defaultTextICSS}>
+        {props.children}
+      </Piv>
+    )
+  }
 }
 
 const defaultTextICSS = cacheFn(() =>
--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -4,11 +4,11 @@ import { type KitProps, useKitProps } from "../createKit"
 import { renderHTMLDOM } from "../piv"
 import { icssTitle } from "../styles"
 import type { Accessify } from "../utils"
-import { Text, type TextRawProps } from "./Text"
+import { Text, type TextProps } from "./Text"
 
 export type TitleProps = Omit<
-  TextRawProps & {
-    htmlAs?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6"
+  TextProps & {
+    as?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6"
   },
   "children"
 >
@@ -20,12 +20,12 @@ export type TitleKitProps = KitProps<
 >
 
 export function Title(kitProps: TitleKitProps) {
-  const { props } = useKitProps(kitProps, { name: "Title", defaultProps: { htmlAs: "h2" } })
+  const { props } = useKitProps(kitProps, { name: "Title", defaultProps: { as: "h2" } })
   const id = createMemo(() => shrinkFn(props.children) ?? "")
   return (
     <Text
       id={id()}
-      defineSelf={(selfProps) => renderHTMLDOM(props.htmlAs, selfProps)}
+      defineSelf={(selfProps) => renderHTMLDOM(props.as, selfProps)}
       shadowProps={props}
       icss={[icssTitle, { marginBottom: "16px" }]}
     >
