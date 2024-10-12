347e3133232c9c3b65b057fe1f6329630e2471b1
edsolater <edsolater@qq.com>
2024-10-12 18:27:46 +0800

    🚚 `createISignal` => `createSignal`

--- src/hooks/createISignal.ts
+++ src/hooks/createISignal.ts
@@ -1,5 +1,5 @@
 import { shrinkFn } from "@edsolater/fnkit"
-import { Accessor, Setter, createSignal } from "solid-js"
+import { Accessor, Setter, createSignal as createSolidjsSignal } from "solid-js"
 
 export type SignalPlugin<V> = () => {
   defaultSignalValue?: (getOriginalValue: () => V | Accessor<V>) => () => V | Accessor<V>
@@ -13,11 +13,11 @@ export type SignalPlugin<V> = () => {
  * with plugin, options
  */
 // TODO: haven't test
-// TODO: maybe it's too complicated
-export function createISignal<V>(
+export function createSignal<V>(
   defaultValue: V | (() => V),
   options?: { name?: string; plugins?: SignalPlugin<V>[] },
-) {
+): [Accessor<V>, Setter<V>] {
+  //#region ---------------- plugin setting collector ----------------
   const defaultSignalValueWrappers: ((getOriginalValue: () => V | Accessor<V>) => () => V | Accessor<V>)[] = []
   const setWrappers: ((originalSet: Setter<V>) => Setter<V>)[] = []
   const getWrappers: ((originalGet: Accessor<V>) => Accessor<V>)[] = []
@@ -29,12 +29,17 @@ export function createISignal<V>(
       get && getWrappers.push(get)
     }
   }
+  //#endregion
+
+  //#region ---------------- accessor\setter collector ----------------
   const wrappedDefaultSignalValue = defaultSignalValueWrappers.reduce(
     (acc, wrapper) => wrapper(acc),
     () => defaultValue,
   )
-  const [get, set] = createSignal<V>(shrinkFn(wrappedDefaultSignalValue()))
+  const [get, set] = createSolidjsSignal<V>(shrinkFn(wrappedDefaultSignalValue()))
   const wrappedGet = getWrappers.reduce((acc, wrapper) => wrapper(acc), get)
   const wrappedSet = setWrappers.reduce((acc, wrapper) => wrapper(acc), set)
-  return [wrappedGet, wrappedSet] as const
+  //#endregion
+
+  return [wrappedGet, wrappedSet]
 }
