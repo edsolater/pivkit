8059f9f2ffa6d3e04d7ab0086260f8b45de77e83
edsolater <edsolater@gmail.com>
2024-10-23 23:04:52 +0800

    ✨ `useInterval` can runImmediate

--- src/hooks/useInterval.ts
+++ src/hooks/useInterval.ts
@@ -1,5 +1,7 @@
-import { setIntervalWithSecondes, setTimeoutWithSecondes } from "@edsolater/fnkit"
+import { isTimeSignal, setInterval, setTimeoutWithSecondes, type TimeSignal } from "@edsolater/fnkit"
 import { onCleanup, onMount } from "solid-js"
+
+type Options = { interval?: TimeSignal; delay?: TimeSignal; runImmediate?: boolean }
 
 /**
  * **DOM API (setInterval)**
@@ -7,16 +9,22 @@ import { onCleanup, onMount } from "solid-js"
  * use seconds , not milliseconds \
  * will auto clear when component unmount
  */
-export function useInterval(callback: () => void, s = 1, delay?: number) {
+export function useInterval(
+  ...args: [callback: () => void, s?: TimeSignal, delay?: TimeSignal] | [callback: () => void, options?: Options]
+) {
+  const callback = args[0]
+  const options = (
+    args.length === 2 || isTimeSignal(args[1]) ? (args[1] ?? 1) : { interval: args[1] ?? 1, delay: args[2] }
+  ) as Options & { interval: TimeSignal }
   onMount(() => {
-    if (delay) {
+    if (options.delay) {
       setTimeoutWithSecondes(() => {
-        const intervalId = setIntervalWithSecondes(callback, s)
-        onCleanup(() => clearInterval(intervalId))
-      }, delay)
+        const { cancel } = setInterval(callback, options)
+        onCleanup(cancel)
+      }, options.delay)
     } else {
-      const intervalId = setIntervalWithSecondes(callback, s)
-      onCleanup(() => clearInterval(intervalId))
+      const { cancel } = setInterval(callback, options)
+      onCleanup(cancel)
     }
   })
 }
