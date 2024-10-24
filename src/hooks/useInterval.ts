f60dc7f15338081678cc2c898e66c575b2ab5f63
edsolater <edsolater@gmail.com>
2024-10-24 12:10:11 +0800

    🚀 v0.0.34

--- src/hooks/useInterval.ts
+++ src/hooks/useInterval.ts
@@ -1,5 +1,5 @@
-import { isTimeSignal, setInterval, setTimeoutWithSecondes, type TimeSignal } from "@edsolater/fnkit"
-import { onCleanup, onMount } from "solid-js"
+import { isTimeSignal, setInterval, setTimeout, type TimeSignal } from "@edsolater/fnkit";
+import { onCleanup, onMount } from "solid-js";
 
 type Options = { interval?: TimeSignal; delay?: TimeSignal; runImmediate?: boolean }
 
@@ -14,14 +14,17 @@ export function useInterval(
 ) {
   const callback = args[0]
   const options = (
-    args.length === 2 || isTimeSignal(args[1]) ? (args[1] ?? 1) : { interval: args[1] ?? 1, delay: args[2] }
+    args.length > 2 || isTimeSignal(args[1]) ? { interval: args[1] ?? 1, delay: args[2] } : (args[1] ?? { interval: 1 })
   ) as Options & { interval: TimeSignal }
   onMount(() => {
     if (options.delay) {
-      setTimeoutWithSecondes(() => {
-        const { cancel } = setInterval(callback, options)
-        onCleanup(cancel)
-      }, options.delay)
+      setTimeout(
+        () => {
+          const { cancel } = setInterval(callback, options)
+          onCleanup(cancel)
+        },
+        { delay: options.delay },
+      )
     } else {
       const { cancel } = setInterval(callback, options)
       onCleanup(cancel)
