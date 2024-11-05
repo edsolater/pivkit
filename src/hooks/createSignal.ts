5d54d1b3f5957b5a14fb22365ad65e7e62cd37b0
edsolater <edsolater@gmail.com>
2024-11-05 23:52:52 +0800

    🚧 `createSignal` have original options

--- src/hooks/createSignal.ts
+++ src/hooks/createSignal.ts
@@ -1,5 +1,5 @@
 import { shrinkFn, wrapArr, type MayArray } from "@edsolater/fnkit"
-import { Accessor, Setter, createSignal as createSolidjsSignal } from "solid-js"
+import { Accessor, Setter, createSignal as createSolidjsSignal, SignalOptions as SolidjsSignalOptions } from "solid-js"
 
 type MayAccessor<V> = V | Accessor<V>
 
@@ -9,6 +9,10 @@ export type SignalPlugin<V> = () => {
   get?: (originalGet: Accessor<V>) => Accessor<V>
 }
 
+type CreateSignalOptions<V> = {
+  name?: string
+  plugins?: MayArray<SignalPlugin<V>>
+} & SolidjsSignalOptions<V>
 
 /**
  * basic util: plugin can get multi features
@@ -19,11 +23,11 @@ export type SignalPlugin<V> = () => {
 export function createSignal<V>(): [Accessor<V | undefined>, Setter<V | undefined>]
 export function createSignal<V>(
   defaultValue: MayAccessor<V>,
-  options?: { name?: string; plugins?: MayArray<SignalPlugin<V>> },
+  options?: CreateSignalOptions<V>,
 ): [Accessor<V>, Setter<V>]
 export function createSignal<V>(
   defaultValue?: MayAccessor<V | undefined>,
-  options?: { name?: string; plugins?: MayArray<SignalPlugin<V | undefined>> },
+  options?: CreateSignalOptions<V | undefined>,
 ): [Accessor<V | undefined>, Setter<V | undefined>] {
   //#region ---------------- plugin setting collector ----------------
   const defaultValueWrappers: Array<
@@ -46,7 +50,7 @@ export function createSignal<V>(
     (acc, wrapper) => wrapper(acc),
     () => defaultValue,
   )
-  const [get, set] = createSolidjsSignal(shrinkFn(wrappedDefaultSignalValue()))
+  const [get, set] = createSolidjsSignal(shrinkFn(wrappedDefaultSignalValue()), options)
   const wrappedGet = getWrappers.reduce((acc, wrapper) => wrapper(acc), get)
   const wrappedSet = setWrappers.reduce((acc, wrapper) => wrapper(acc), set)
   //#endregion
